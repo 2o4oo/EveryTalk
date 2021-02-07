b1426ca93fd7512fe86f9505386d2a5e7807b3d7
ByeongjaeKang <kangbj@pm.me>
2021-02-07 16:14:42 +0900

    Calibrating touch panel..

--- everytalk.ino
+++ everytalk.ino
@@ -1,4 +1,5 @@
 #include "SPI.h"
+#include <TinyGPS.h>
 #include <ILI9488.h>
 #include <TFT_Touch.h>
 #include <Adafruit_GFX.h>
@@ -38,6 +39,7 @@ int yPerTouch = ( VMAX - VMIN ) / VRES; // Y
 uint8_t currentMenu = 0; // Main menu
 bool isCalibrated = 0;
 
+TinyGPS gps;
 ILI9488 tft = ILI9488(TFT_CS, TFT_DC, TFT_RST);
 TFT_Touch touch = TFT_Touch(DCS, DCLK, DIN, DOUT);
 
@@ -48,53 +50,42 @@ void setup() {
   tft.setRotation(4);
   touch.setCal(HMIN, HMAX, VMIN, VMAX, HRES, VRES, XYSWAP); // Raw xmin, xmax, ymin, ymax, width, height
   touch.setRotation(1);
-  pinMode(Backlight, OUTPUT);
+  pinMode(TFT_BL, OUTPUT);
   tft.fillScreen(ILI9488_BLACK);
   tft.setFont(&FreeSans18pt7b);
-  tft.setTextColor(ILI9488_WHITE);tft.setTextSize(1);
-  tft.setCursor(85,180);tft.print("EveryTalk");
-  tft.setFont();tft.setCursor(122,460);tft.print("made by L0G1C");
+  tft.setTextColor(ILI9488_WHITE); tft.setTextSize(1);
+  tft.setCursor(85, 180); tft.print("EveryTalk");
+  tft.setFont(); tft.setCursor(122, 460); tft.print("made by L0G1C");
   tft.setFont(&FreeSans12pt7b);
-  digitalWrite(Backlight, HIGH);
-}
-
-class ScreenPoint {
-  public:
-  int16_t scr_x;
-  int16_t scr_y;
+  digitalWrite(TFT_BL, HIGH);
 }
 
 void loop(void) {
   caliScr();
 }
 
-void calibrateTS() {
-  tft.drawFastHLine(10,20,20,ILI9488_RED);
-  tft.drawFastVLine(20,10,20,ILI9488_RED);
-  
-}
-
 unsigned long caliScr() {
-  unsigned int X_Raw;
-  unsigned int Y_Raw;
+  unsigned int X1_Raw;
+  unsigned int Y1_Raw;
 
- if (touch.Pressed()) // Note this function updates coordinates stored within library variables
-  { 
-    X_Raw = touch.RawX();
-    Y_Raw = touch.RawY();
+    tft.drawFastHLine(10, VRES - 20, 20, ILI9488_RED); // x=20, y=460
+    tft.drawFastVLine(20, VRES - 30, 20, ILI9488_RED);
 
-    tft.setCursor(1,20);
+    tft.drawFastHLine(HRES - 30, 20, 20, ILI9488_RED); // x=300, y=20
+    tft.drawFastVLine(HRES - 20, 10, 20, ILI9488_RED);
+
+  while (touch.Pressed()) // Note this function updates coordinates stored within library variables
+  {
+    X1_Raw = touch.RawX();
+    Y1_Raw = touch.RawY(); // #1 Raw TS value, (20, 20)
+
+    tft.setCursor(1, 20);
     tft.fillScreen(0xFFFF);
     tft.setTextColor(ILI9488_BLACK);
-    tft.print(X_Raw);
+    tft.print(X1_Raw);
     tft.print(", ");
-    tft.print(Y_Raw);
-    
-    tft.setCursor(1,50);
-    tft.print(( X_Raw - HMIN ) / xPerTouch);
-    tft.print(", ");
-    tft.print(480 - ( ( Y_Raw - VMIN ) / yPerTouch));
-    tft.drawPixel( ( X_Raw - HMIN ) / xPerTouch, 480 - ( ( Y_Raw - VMIN ) / yPerTouch) , ILI9488_RED );
+    tft.print(Y1_Raw);
+
     delay(10);
   }
 }
